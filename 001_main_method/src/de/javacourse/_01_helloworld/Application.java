3c18475782ea4af37af6aae7bc9c04157970885b
Alexander Hagenhoff <a.hagenhoff@rebuy.de>
2014-07-04 00:51:09 +0200

    Add first simple examples

---
+++ 001_main_method/src/de/javacourse/_01_helloworld/Application.java
@@ -0,0 +1,9 @@
+package de.javacourse._01_helloworld;
+
+public class Application
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hallo Welt");
+    }
+}
---
+++ 001_main_method/src/de/javacourse/_02_helloword_int/ApplicationWithInt.java
@@ -0,0 +1,10 @@
+package de.javacourse._02_helloword_int;
+
+public class ApplicationWithInt
+{
+    public static void main(String[] args)
+    {
+        int value = 10;
+        System.out.println("Hallo Welt " + value);
+    }
+}
