f45905a0317aec8bc86e794c5fae1600972252d3
Mrs-chaos <caro.horsch@gmail.com>
2015-02-15 11:38:10 +0100

    rules + Test

--- gameoflife/src/de/javacourse/Rules.java
+++ gameoflife/src/de/javacourse/Rules.java
@@ -1,9 +1,22 @@
 package de.javacourse;
 
+
 public class Rules
 {
     public boolean isAliveFuture(Cell cell, int neighboursAliveCount)
     {
+       if (!cell.isAlivePresent()){
+           return neighboursAliveCount == 3;
+       }
+
+        if(cell.isAlivePresent()){
+            if(neighboursAliveCount<2||neighboursAliveCount>3){
+                return false;
+            }else{
+                return true;
+            }
+        }
+
         return false;
     }
 }
